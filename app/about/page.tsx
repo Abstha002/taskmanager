ae257e5f6c0f7adf36d230115024679c1723c7c2
Abstha002 <ab.stha002@proton.me>
2025-06-07 18:14:04 +0545

    title update

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <>
+        <h1>This is about page</h1>
+        <p>This simple task manager is built with Next.js 14, React Context API, and TypeScript using the App Router.</p>
+        </>
+    )
+}
